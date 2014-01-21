5d87888a3d11c710e283af7d218f11fde4991577
oasisweng <dingzhong.weng@lycheestudio.co.uk>
2014-01-21 23:09:29 +0000

    Task1.3 added, robot radius optimized!

--- robotTemplate.h
+++ robotTemplate.h
@@ -5,13 +5,13 @@ typedef int bool;
 #define true 1
 #define false 0
 
-#define ERROR_MAX 1
+#define ERROR_MAX 0
 
 #define COUNTS_PER_DEGREE 2.347 //845/360
-#define ROBOT_RADIUS 134.915 //845/2Pi
+#define ROBOT_RADIUS 135.5 //845/2Pi ~~ 134.915, but 135.5 gives best performance on simulator
 #define Pi 3.145926
 
-//#define DEBUG
+#define DEBUG
 
 typedef struct Mission
 {
